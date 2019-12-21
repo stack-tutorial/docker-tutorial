bff984b7b4c6f1867795a4ed602eb53bb708e991
Tosone <i@tosone.cn>
2019-12-22 00:00:58 +0800

    Fix postgres and mongo startup.

--- postgres/gen.sh
+++ postgres/gen.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -euo pipefail
-
 openssl req -new -text -newkey rsa:4096 -passout pass:${SECRETPERSONAL} -subj /CN=localhost -keyout privkey.pem -out server.req
 openssl rsa -in privkey.pem -passin pass:${SECRETPERSONAL} -out server.key
 openssl req -x509 -in server.req -text -key server.key -out server.crt
